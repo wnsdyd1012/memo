20a2632600f6e34af1c9f6ee35f6dc10502179a3
wnsdyd1012 <wnsdyd1012@gmail.com>
2023-08-08 16:27:53 +0900

    메모장
    완

---
+++ static/app.js
@@ -0,0 +1,79 @@
+function displayMemo(memo) {
+  const ul = document.querySelector("#memo-ul");
+
+  const li = document.createElement("li");
+  li.innerText = `${memo.content}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "삭제";
+  deleteBtn.addEventListener("click", deleteMemo);
+  deleteBtn.dataset.id = memo.id;
+
+  li.appendChild(editBtn);
+  li.appendChild(deleteBtn);
+  ul.appendChild(li);
+}
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id;
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE",
+  });
+  readMemo();
+}
+
+async function readMemo() {
+  const res = await fetch("/memos");
+  const jsonRes = await res.json();
+  const ul = document.querySelector("#memo-ul");
+  ul.innerHTML = "";
+  jsonRes.forEach(displayMemo);
+}
+
+async function createMemo(value) {
+  const res = await fetch("/memos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: "" + new Date().getTime(),
+      content: value,
+    }),
+  });
+  readMemo();
+}
+
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  const editInput = prompt("수정할 값을 입력하세요");
+  const res = await fetch(`/memo/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id,
+      content: editInput,
+    }),
+  });
+  readMemo();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = document.querySelector("#memo-input");
+  console.log(input.value);
+  createMemo(input.value);
+  input.value = "";
+}
+
+const form = document.querySelector("#memo-form");
+form.addEventListener("submit", handleSubmit);
+
+readMemo();
